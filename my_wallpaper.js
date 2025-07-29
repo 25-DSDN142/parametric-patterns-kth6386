8970a017b58c816a030c36391ef49c86456a6463
kth6386 <kth6386@gmail.com>
2025-07-29 12:14:03 +1200

    Week4
    trying to change the circle to a petal shape but struggling

--- my_wallpaper.js
+++ my_wallpaper.js
@@ -8,11 +8,11 @@ let numPetals = 8;
 let innerRadius = 10; 
 let outerRadius = 50; 
 
-
+//if //requirement
 
 function setup_wallpaper(pWallpaper) {
   pWallpaper.output_mode(DEVELOP_GLYPH);
-  //pWallpaper.output_mode(GRID_WALLPAPER);
+  //pWallpaper.output_mode(GRID_WALLPAPER);  //DEVELOP_GLYPH
   
   pWallpaper.resolution(FIT_TO_SCREEN);
   pWallpaper.show_guide(true); //set this to false when you're ready to print
@@ -24,7 +24,7 @@ function setup_wallpaper(pWallpaper) {
 
 
   angleMode(DEGREES);
-  noLoop(); //makes the draw fuction run once
+  //noLoop(); //makes the draw fuction run once
 
 }
 
@@ -32,10 +32,13 @@ function wallpaper_background() {
   background(240, 230, 210); //light orange (neutral colour)
 }
 
+
+
 function my_symbol() { // do not rename this function. Treat this similarly to a Draw function
 
   translate(midX, midY); //makes the origin in the middle of the canvas
   drawDancheongPattern();
+
 
 }
 
@@ -47,14 +50,29 @@ function drawDancheongPattern() {
 
   push();
 
-  //outer turquoise rotating circle
   rotate(angle);
+
+  //outer turquoise rotating circle
   fill('#2a9d8f');
   ellipse(0, outerRadius, 20, 40);
 
   //inner orange rotating circle
   fill('#e76f51');
   ellipse(0, innerRadius, 10, 20);
+
+
+  fill('red');
+curve(
+  -150 - 50, 275 - 60,   // control point 1
+   50  - 50,  60 - 60,   // start point
+   50  - 50,  60 - 60,   // end point (same as start for this curve)
+  250 - 50, 275 - 60     // control point 2
+);
+
+
+
+
+
 
   pop();
 
@@ -70,4 +88,10 @@ function drawDancheongPattern() {
   stroke('#e9c46a'); // Yellow
   strokeWeight(2);
   circle(0, 0, outerRadius * 2);
+
+
+
+
+
+
 }
